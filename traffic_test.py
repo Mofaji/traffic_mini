7fe787d1f060231546c0bc814c7d7ef2db8d6faf
Mofaji <faizaanfareed2002@gmail.com>
2023-07-12 04:54:44 +0530

    ok

---
+++ traffic_test.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+import pickle
+
+
+#############################################
+
+frameWidth = 640         # CAMERA RESOLUTION
+frameHeight = 480
+brightness = 180
+threshold = 0.75         # PROBABILITY THRESHOLD
+font = cv2.FONT_HERSHEY_SIMPLEX
+##############################################
+
+# SETUP THE VIDEO CAMERA
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, brightness)
+# IMPORT THE TRAINED MODEL
+pickle_in = open("model_trained.p", "rb")  
+model = pickle.load(pickle_in)
+
+def grayscale(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img
+
+def equalize(img):
+    img = cv2.equalizeHist(img)
+    return img
+
+def preprocessing(img):
+    img = grayscale(img)
+    img = equalize(img)
+    img = img / 255
+    return img
+
+def getClassName(classNo):
+    if classNo == 0: return 'Speed Limit 20 km/h'
+    elif classNo == 1: return 'Speed Limit 30 km/h'
+    elif classNo == 2: return 'Speed Limit 50 km/h'
+    # ... rest of the class names ...
+
+while True:
+    # READ IMAGE
+    success, imgOriginal = cap.read()
+
+    # PROCESS IMAGE
+    img = np.asarray(imgOriginal)
+    img = cv2.resize(img, (32, 32))
+    img = preprocessing(img)
+    cv2.imshow("Processed Image", img)
+    img = img.reshape(1, 32, 32, 1)
+    cv2.putText(imgOriginal, "CLASS: ", (20, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(imgOriginal, "PROBABILITY: ", (20, 75), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+
+    # PREDICT IMAGE
+    predictions = model.predict(img)
+    classIndex = model.predict_classes(img)
+    probabilityValue = np.amax(predictions)
+    if probabilityValue > threshold:
+        cv2.putText(imgOriginal, str(classIndex) + " " + str(getClassName(classIndex)), (120, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(imgOriginal, str(round(probabilityValue * 100, 2)) + "%", (180, 75), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+    cv2.imshow("Result", imgOriginal)
+
+    if cv2.waitKey(1) and 0xFF == ord('q'):
+        break
